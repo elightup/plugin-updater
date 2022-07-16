a78b41898e145f0d0ab3c4261522dfca2d5c9973
LyNguyen <cnttnt@gmail.com>
2022-07-16 15:55:53 +0700

    PR #3: Do not show license key when inspect element

--- src/Settings.php
+++ src/Settings.php
@@ -10,6 +10,7 @@ class Settings {
 		$this->manager = $manager;
 		$this->checker = $checker;
 		$this->option  = $option;
+		$this->fake_api_key  = "****************";
 	}
 
 	public function setup() {
@@ -75,6 +76,10 @@ class Settings {
 		];
 		$status      = $this->option->get_license_status();
 		$license_key = $this->option->get_license_key();
+
+		if ( 'active' === $status ) {
+			$license_key = $this->fake_api_key;
+		}
 		?>
 		<input class="regular-text" name="<?= esc_attr( $this->manager->option_name ) ?>[api_key]" value="<?= esc_attr( $license_key ) ?>" type="password" autocomplete="autocomplete_off_randString">
 		<?php if ( isset( $messages[ $status ] ) ) : ?>
@@ -101,6 +106,9 @@ class Settings {
 		// Do nothing if license key remains the same.
 		$prev_key = $this->option->get_license_key();
 		if ( isset( $option['api_key'] ) && $option['api_key'] === $prev_key ) {
+			return;
+		}
+		if ( $this->fake_api_key === $option['api_key'] ) {
 			return;
 		}
 
